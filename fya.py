7630d508970d55a45c91a5115a6fc7034364ef54
Jakob Hoeper <jhoeper@bbaw.de>
2018-04-10 21:02:38 +0200

    define components of complex key for identification of lemma entries in output structure

--- fya.py
+++ fya.py
@@ -10,14 +10,25 @@ tree = etree.parse('dummy.xml')
 body=etree.Element('body')
 output=etree.ElementTree(body)
 
-key_parts = ["[gramGrp/pos='{}']", "[usg='{}']"]
+key_parts = ["gramGrp/pos", "usg"]
 
-for e in tree.iter('entry'):
+for e in tree.xpath(".//entry[form/@type='lemma']"):
+    # extract lemma base form
     lemma = e.find("form[@type='lemma']").text
-    output_entry = output.find("entry[@lemma='{}']".format(lemma)) or etree.SubElement(body, 'entry', attrib={'lemma': lemma})
+    # look up lemma base entry in output tree, create a copy of current element if none is found
+    query = './/entry' + ''.join(["[{}]".format(key+"='{}'".format(e.xpath(key)[0].text)) for key in key_parts] )
+    query += "[form/@type='lemma'][form='{}']".format(lemma)
+    print(query)
+    output_entry = output.xpath(query)
+    if len(output_entry) < 1:
+        output_entry = deepcopy(e)
+        output.getroot().append(output_entry)
+    else:
+        output_entry = output_entry[0]
+        for a in e.findall("form[@type='non_lemma']"):
+            output_entry.append(a)
     print('lemma form {}, element in output tree: {}'.format(lemma, output_entry))
-    for a in e.findall("form[@type='non_lemma']"):
-        output_entry.append(a)
+
 
 output.write('out.xml')
         
