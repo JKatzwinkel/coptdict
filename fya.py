827398469e08d28d91794a386a2aa137db682171
Jakob Hoeper <jhoeper@bbaw.de>
2018-04-10 21:08:10 +0200

    clean up a bit

--- fya.py
+++ fya.py
@@ -7,28 +7,28 @@ from copy import deepcopy
 tree = etree.parse('dummy.xml')
 
 #output tree
-body=etree.Element('body')
-output=etree.ElementTree(body)
+body = etree.Element('body')
+output = etree.ElementTree(body)
 
 key_parts = ["gramGrp/pos", "usg"]
 
 for e in tree.xpath(".//entry[form/@type='lemma']"):
     # extract lemma base form
     lemma = e.find("form[@type='lemma']").text
-    # look up lemma base entry in output tree, create a copy of current element if none is found
+    # look up lemma base entry in output tree
     query = './/entry' + ''.join(["[{}]".format(key+"='{}'".format(e.xpath(key)[0].text)) for key in key_parts] )
     query += "[form/@type='lemma'][form='{}']".format(lemma)
-    print(query)
+    # run query on output lemma entry list
     output_entry = output.xpath(query)
+    # if lemma has no entry element in output yet, create one by copying current element from wordlist
     if len(output_entry) < 1:
         output_entry = deepcopy(e)
         output.getroot().append(output_entry)
     else:
+        # if lemma is already in output list, add specific flexions from current wordlist element
         output_entry = output_entry[0]
         for a in e.findall("form[@type='non_lemma']"):
             output_entry.append(a)
-    print('lemma form {}, element in output tree: {}'.format(lemma, output_entry))
-
 
 output.write('out.xml')
         
