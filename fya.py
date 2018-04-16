0000f0b56d9773bc92481999e41a06e4adbe889e
Jakob Hoeper <jhoeper@bbaw.de>
2018-04-16 18:52:07 +0200

    apply key-to-query logic to nested level of sub elements

--- fya.py
+++ fya.py
@@ -10,25 +10,34 @@ tree = etree.parse('dummy.xml')
 body = etree.Element('body')
 output = etree.ElementTree(body)
 
-key_parts = ["gramGrp/pos", "usg"]
+key_parts = ["gramGrp/pos"]
 
-for e in tree.xpath(".//entry[form/@type='lemma']"):
+components = {
+            "form": ["orth", "usg"],
+            "sense": ["cit/quote"]
+            }
+
+for entry in tree.xpath(".//entry[form/@type='lemma']"):
     # extract lemma base form
-    lemma = e.find("form[@type='lemma']").text
+    lemma = entry.find("form[@type='lemma']").find("orth").text
     # look up lemma base entry in output tree
-    query = './/entry' + ''.join(["[{}]".format(key+"='{}'".format(e.xpath(key)[0].text)) for key in key_parts] )
-    query += "[form/@type='lemma'][form='{}']".format(lemma)
+    query = './/entry' + ''.join(["[{}]".format(key+"='{}'".format(entry.xpath(key)[0].text)) for key in key_parts] )
+    query += "[form/@type='lemma'][form/orth='{}']".format(lemma)
     # run query on output lemma entry list
     output_entry = output.xpath(query)
     # if lemma has no entry element in output yet, create one by copying current element from wordlist
     if len(output_entry) < 1:
-        output_entry = deepcopy(e)
+        output_entry = deepcopy(entry)
         output.getroot().append(output_entry)
     else:
-        # if lemma is already in output list, add specific flexions from current wordlist element
+        # if lemma is already in output list, add specific flexions and shit like that from current wordlist element
         output_entry = output_entry[0]
-        for a in e.findall("form[@type='non_lemma']"):
-            output_entry.append(a)
+        for elemname, criteria in components.items():
+            for e in entry.xpath(elemname):
+                query = ''.join(["[{}]".format(key+"='{}'".format(e.xpath(key)[0].text)) for key in criteria if len(e.xpath(key)) > 0 ] )
+                if len(output_entry.xpath(elemname + query)) < 1:
+                    output_entry.append(e)
+
 
 output.write('out.xml')
         
